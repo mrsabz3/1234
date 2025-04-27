2d4603f2f4d266363bc76ac4df16647d9f7ae114
mrsabz3 <sabakobaidze333@gmail.com>
2025-04-27 19:55:01 +0400

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ import { Testimonials } from "@/components/landing/Testimonials";
 import { Pricing } from "@/components/landing/Pricing";
 
 
-console.log("what")
+console.log("wha1234t")
 
 export default function Home() {
   // Add padding to body when the component mounts
