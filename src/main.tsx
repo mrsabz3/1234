e4e61b590696d90bd1bc01e69efc4304a18a7ba9
mrsabz3 <sabakobaidze333@gmail.com>
2025-04-28 13:56:49 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -45,7 +45,7 @@ const router = createBrowserRouter([
   },
 ]);
 
-console.log("Whsat")
+console.log("Wh2sat")
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
